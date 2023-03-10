21b575c340a63cb3e0a0bedd798d576117cd0b29
rbiou <45234453+rbiou@users.noreply.github.com>
2023-03-11 00:51:13 +0100

    Add telegram config

--- config.py
+++ config.py
@@ -5,8 +5,13 @@ Holds the settings for the Vinted-Alert
 """
 
 # vinted
-URL = "https://www.vinted.fr/vetements?search_text=...&order=newest_first" # Your personnal research. !!! Important => &order=newest_first at the end 
+URL = "https://www.vinted.fr/catalog?search_text=matelas&brand_id[]=391549&brand_id[]=1323679&brand_id[]=493960&currency=EUR&search_id=8282966973&order=newest_first" # Your personnal research. !!! Important => &order=newest_first at the end 
 
 # telegram
-TELEGRAM_KEY = "" # API key for your telegram bot
-CHAT_ID = "" # Your chat id
+TELEGRAM_KEY = "MIIBCgKCAQEA6LszBcC1LGzyr992NzE0ieY+BSaOW622Aa9Bd4ZHLl+TuFQ4lo4g
+5nKaMBwK/BIb9xUfg0Q29/2mgIR6Zr9krM7HjuIcCzFvDtr+L0GQjae9H0pRB2OO
+62cECs5HKhT5DZ98K33vmWiLowc621dQuwKWSQKjWf50XYFw42h21P2KXUGyp2y/
++aEyZ+uVgLLQbRA1dEjSDZ2iGRy12Mk5gpYc397aYp438fsJoHIgJ2lgMv5h7WY9
+t6N/byY9Nw9p21Og3AoXSL2q/2IJ1WRUhebgAdGVMlV1fkuOQoEzR7EdpqtQD9Cs
+5+bfo3Nhmcyvk5ftB0WkJ9z6bNZ7yxrP8wIDAQAB" # API key for your telegram bot
+CHAT_ID = "-1001918849175" # Your chat id
