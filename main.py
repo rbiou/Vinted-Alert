dae18f9d7ead9301b573c2dfa4b33748225910ba
rbiou <MmF6yhSeGIT>
2023-03-11 03:44:41 +0100

    Add all config and edit notification system

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from traceback import print_exc
 from random import randint, random
 
 from requests import get
+from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 
 import config
@@ -19,71 +20,49 @@ from utils import log
 from notify import send_notification
 from constants import NOTIFICATION_CONTENT
 
-def update_data(items, notification):
-    """Main function of the program"""
-
-    # current_items = []
-
-    response = get(config.URL)
-    response.raise_for_status() # exception catched in the main loop
-    soup = BeautifulSoup(response.text, 'lxml') # lxml is faster but a dependency, "html.parser" is quite fast and installed by default
-    script = soup.find_all('script')[47] # careful with this as it might change at any update
-    data = json.loads(script.string) # might check the type="application/json"
-    data = data['items']['byId'].values()
-    # log(data, "Vinted") # --> debug
-    
-    for item in data:
-        # Collect data
-        title = item.get("title", "N/A")
-        description = item.get("description", "No description")
-        original_price = item.get("original_price_numeric", -1) # snake cased?!
-        actual_price = item.get("price_numeric", -1) # digits
-        # price = item.get("price", "ERR") # Format 4,00 €
-        brand = item.get("brand", "N/A")
-        images = item.get("photos", "No Photo") # should it be an URL --> provide a "Not Found URL"
-        login = item.get("user", {}).get("login", "N/A")
-        number_of_positive_feedback = item.get("user", {}).get("positive_feedback_count", -1)
-        number_of_neutral_feedback = item.get("user", {}).get("neutral_feedback_count", -1)
-        number_of_negative_feedback = item.get("user", {}).get("negative_feedback_count", -1)
-        # feedback_reputation = item['user']['feedback_reputation']
-        # created_at = item['user']['created_at']
-        # last_login = item['user']['last_loged_on_ts']
-        last_log_on = item.get("user", {}).get("last_loged_on", "Error") # Format type : "Hier à 19h"
-        url = item.get("url", "N/A") # provide not found as default
-        status = item.get("status", "N/A")
-        item = "{login} - {title} {actual_price} € : {description}".format(login=login, title=title, actual_price=actual_price, description=description)
-        # Save the item
-        # current_items.append(item)
-        if item not in items:
-            log("New item : {item} => {url}".format(item=item, url=url), domain="Vinted")
-            content = NOTIFICATION_CONTENT.format(
-                actual_price = actual_price,
-                title = title,
-                description = description,
-                status = status,
-                brand = brand,
-                original_price = original_price,
-                login = login,
-                last_log_on = str(last_log_on).lower(),
-                positive_feedback = number_of_positive_feedback,
-                neutral_feedback = number_of_neutral_feedback,
-                negative_feedback = number_of_negative_feedback,
-                url = url
-            )
-            # Send notification
-            send_notification(content, images, notification)
-            items.append(item)
-        else:
-            log("{item} => {url}".format(item=item, url=url), domain="Vinted")
-
+from datetime import datetime, timezone
 
 if __name__ == "__main__":
-    items = []
-    notification = False
-    while True:
-        try:
-            update_data(items, notification)
-        except Exception:
-            print_exc()
-        notification = True
-        sleep(randint(60, 120) + random())
+    try:
+        req = Request(url=config.URL, headers={'User-Agent': 'Mozilla/5.0'})
+        response = urlopen(req).read()
+        soup = BeautifulSoup(response.decode('utf-8'), 'lxml') # lxml is faster but a dependency, "html.parser" is quite fast and installed by default
+        script = soup.find_all('script')[41] # careful with this as it might change at any update
+        data = json.loads(script.string) # might check the type="application/json"
+        data = data['items']['catalogItems']['byId'].values()
+        # log(data, "Vinted") # --> debug
+        
+        for item in data:
+            # Collect data
+            title = item.get("title", "N/A")
+            price = item.get("price", "ERR") # Format 4,00 €
+            brand = item.get("brand_title", "N/A")
+            login = item.get("user", {}).get("login", "N/A")
+            url = item.get("url", "N/A") # provide not found as default
+            item = "{login} - {title} {price} €".format(login=login, title=title, price=price)
+	    	# Get more details about the item
+            reqDetails = Request(url=url, headers={'User-Agent': 'Mozilla/5.0'})
+            responseDetails = urlopen(reqDetails).read()
+            soupDetails = BeautifulSoup(responseDetails.decode('utf-8'), 'lxml')
+            scriptDetails = soupDetails.find_all('script', {"data-component-name": "ItemPushUpButton"})[0] # careful with this as it might change at any update
+            dataDetails = json.loads(scriptDetails.string) # might check the type="application/json"
+            dataDetails = dataDetails["item"]
+            images = dataDetails.get("photos", "No Photo")
+            feedback_reputation = dataDetails['user']['feedback_reputation']
+            created_at = datetime.strptime(dataDetails['created_at_ts'], '%Y-%m-%dT%H:%M:%S%z')
+            # Notify if new item from last ten min
+            if ((datetime.now(timezone.utc) - created_at).seconds / 60 < 11):
+                log("New item : {item} => {url}".format(item=item, url=url), domain="Vinted")
+                content = NOTIFICATION_CONTENT.format(
+                    price = price,
+                    title = title,
+                    brand = brand,
+			    	feedback_reputation = "{:.0%}".format(feedback_reputation) if (feedback_reputation and feedback_reputation > 0) else "Non noté ATM",
+                    url = url
+                )
+                # Send notification
+                send_notification(content, images)
+            else:
+                exit()
+    except Exception:
+        print_exc()
--- notify.py
+++ notify.py
@@ -13,14 +13,13 @@ from config import TELEGRAM_KEY, CHAT_ID
 log("Connecting to the Telegram API", "Telegram")
 bot = Bot(TELEGRAM_KEY)
 
-def send_notification(content, images, notification):
+def send_notification(content, images):
     '''Sends a telegram notification'''
     # log(content, "Telegram")
     # print(images)
-    # print(notification)
-    if notification:       
-        log("Notifying user about a new product", "Telegram")
-        bot.send_message(CHAT_ID, content)
-        # Send the 3 firsts images
-        for image in images[:3]:
-            bot.send_photo(CHAT_ID, image['url'])
+    # print(notification)      
+    log("Notifying user about a new product", "Telegram")
+    bot.send_message(CHAT_ID, content)
+    # Send the 3 firsts images
+    for image in images[:3]:
+        bot.send_photo(CHAT_ID, image["url"])
