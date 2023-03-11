9864b26634b1fa3845bcc7ecd816cd8dbe3227a6
rbiou <MmF6yhSeGIT>
2023-03-11 15:18:26 +0100

    Integrate search vinted URL management to Google Sheet

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from random import randint, random
 from requests import get
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+from pandas import pandas as pd
 
 import config
 from utils import log
@@ -24,47 +25,54 @@ from datetime import datetime, timezone
 
 def sendNotification(request):
     try:
-        req = Request(url=config.URL, headers={'User-Agent': 'Mozilla/5.0'})
-        response = urlopen(req).read()
-        soup = BeautifulSoup(response.decode('utf-8'), 'lxml') # lxml is faster but a dependency, "html.parser" is quite fast and installed by default
-        script = soup.find_all('script', {"data-js-react-on-rails-store": "MainStore"})[0] # careful with this as it might change at any update
-        data = json.loads(script.string) # might check the type="application/json"
-        data = data['items']['catalogItems']['byId'].values()
-        # log(data, "Vinted") # --> debug
-        
-        for item in data:
-            # Collect data
-            title = item.get("title", "N/A")
-            price = item.get("price", "ERR") # Format 4,00 €
-            brand = item.get("brand_title", "N/A")
-            login = item.get("user", {}).get("login", "N/A")
-            url = item.get("url", "N/A") # provide not found as default
-            item = "{login} - {title} {price} €".format(login=login, title=title, price=price)
-     	# Get more details about the item
-            reqDetails = Request(url=url, headers={'User-Agent': 'Mozilla/5.0'})
-            responseDetails = urlopen(reqDetails).read()
-            soupDetails = BeautifulSoup(responseDetails.decode('utf-8'), 'lxml')
-            scriptDetails = soupDetails.find_all('script', {"data-component-name": "ItemPushUpButton"})[0] # careful with this as it might change at any update
-            dataDetails = json.loads(scriptDetails.string) # might check the type="application/json"
-            dataDetails = dataDetails["item"]
-            images = dataDetails.get("photos", "No Photo")
-            feedback_reputation = dataDetails['user']['feedback_reputation']
-            created_at = datetime.strptime(dataDetails['created_at_ts'], '%Y-%m-%dT%H:%M:%S%z')
-            # Notify if new item from last ten min
-            if ((datetime.now(timezone.utc) - created_at).seconds / 60 < 11):
-                log("New item : {item} => {url}".format(item=item, url=url), domain="Vinted")
-                content = NOTIFICATION_CONTENT.format(
-                    price = price,
-                    title = title,
-                    brand = brand,
-       	feedback_reputation = "{:.0%}".format(feedback_reputation) if (feedback_reputation and feedback_reputation > 0) else "Non noté ATM",
-                    url = url
-                )
-                # Send notification
-                send_notification(content, images)
-            else:
-                exit()
+	    #Read datas
+        data = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRIrg56uMRWI3zLci_4FHbA3chxVjnDtucBFhnMxV5sAWkQ4OPXlYkH6jZUTVl-GEUp61ZEowsWlZhc/pub?gid=0&single=true&output=csv", on_bad_lines="skip")
+	    
+        #For each alert requested, check event and deals
+        for index, search in data.iterrows():
+            log("New search to alert : {url}".format(url=search["url"]), domain="Vinted")
+            req = Request(url=search["url"], headers={'User-Agent': 'Mozilla/5.0'})
+            response = urlopen(req).read()
+            soup = BeautifulSoup(response.decode('utf-8'), 'lxml') # lxml is faster but a dependency, "html.parser" is quite fast and installed by default
+            script = soup.find_all('script', {"data-js-react-on-rails-store": "MainStore"})[0] # careful with this as it might change at any update
+            data = json.loads(script.string) # might check the type="application/json"
+            data = data['items']['catalogItems']['byId'].values()
+            # log(data, "Vinted") # --> debug
+            
+            for item in data:
+                # Collect data
+                title = item.get("title", "N/A")
+                price = item.get("price", "ERR") # Format 4,00 €
+                brand = item.get("brand_title", "N/A")
+                login = item.get("user", {}).get("login", "N/A")
+                url = item.get("url", "N/A") # provide not found as default
+                item = "{login} - {title} {price} €".format(login=login, title=title, price=price)
+     	    # Get more details about the item
+                reqDetails = Request(url=url, headers={'User-Agent': 'Mozilla/5.0'})
+                responseDetails = urlopen(reqDetails).read()
+                soupDetails = BeautifulSoup(responseDetails.decode('utf-8'), 'lxml')
+                scriptDetails = soupDetails.find_all('script', {"data-component-name": "ItemPushUpButton"})[0] # careful with this as it might change at any update
+                dataDetails = json.loads(scriptDetails.string) # might check the type="application/json"
+                dataDetails = dataDetails["item"]
+                images = dataDetails.get("photos", "No Photo")
+                feedback_reputation = dataDetails['user']['feedback_reputation']
+                created_at = datetime.strptime(dataDetails['created_at_ts'], '%Y-%m-%dT%H:%M:%S%z')
+                # Notify if new item from last ten min
+                if ((datetime.now(timezone.utc) - created_at).seconds / 60 < 11):
+                    log("New item : {item} => {url}".format(item=item, url=url), domain="Vinted")
+                    content = NOTIFICATION_CONTENT.format(
+                        price = price,
+                        title = title,
+                        brand = brand,
+       	                feedback_reputation = "{:.0%}".format(feedback_reputation) if (feedback_reputation and feedback_reputation > 0) else "Non noté ATM",
+                        url = url
+                    )
+                    # Send notification
+                    send_notification(content, images)
+                else:
+                    exit()
         return ('', 200)
     except Exception:
         print_exc()
         return ('', 500)
+sendNotification(True)
--- notify.py
+++ notify.py
@@ -6,9 +6,11 @@ Holds the notification logic, using the Telegram API
 
 from telegram import *
 from telegram.ext import *
-
+from telegram.error import RetryAfter, TimedOut
 from utils import log
 from config import TELEGRAM_KEY, CHAT_ID
+
+import time
 
 log("Connecting to the Telegram API", "Telegram")
 bot = Bot(TELEGRAM_KEY)
@@ -19,7 +21,15 @@ def send_notification(content, images):
     # print(images)
     # print(notification)      
     log("Notifying user about a new product", "Telegram")
-    bot.send_message(CHAT_ID, content)
-    # Send the 3 firsts images
-    for image in images[:3]:
-        bot.send_photo(CHAT_ID, image["url"])
+    try:
+        bot.send_message(CHAT_ID, content)
+        # Send the 3 firsts images
+        for image in images[:3]:
+            bot.send_photo(CHAT_ID, image["url"])
+    except RetryAfter:
+        print("ALERT FLOOD : wait 30s")
+        time.sleep(30)
+        send_notification(content, images)
+    except TimedOut:
+        print("TIME OUT : try again")
+        send_notification(content, images)
